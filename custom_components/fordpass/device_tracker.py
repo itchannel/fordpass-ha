f947a9ee744cd4797c9917dc3ddf19385a666bd8
Steve <steve@itchannel.me>
2020-12-12 10:06:29 +1000

    Missing variable

--- custom_components/fordpass/device_tracker.py
+++ custom_components/fordpass/device_tracker.py
@@ -6,7 +6,7 @@ from homeassistant.components.device_tracker.config_entry import TrackerEntity
 
 from . import FordPassEntity
 from .const import DOMAIN
-
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Add the Entities from the config."""
