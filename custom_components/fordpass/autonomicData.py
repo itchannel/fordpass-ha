c126eebe1b44bf1f0bec265c28ec680dda7bdf42
SquidBytes <unstable27@proton.me>
2023-10-09 23:35:28 -0400

    gross script to grab json from autonomic API

---
+++ custom_components/fordpass/autonomicData.py
@@ -0,0 +1,80 @@
+import json
+import requests
+
+fp_username = "" #FordPass Username
+fp_password = "" #FordPass password
+fp_vin = "" #FordPass VIN for vehicle to get data from
+fp_token = "Hass_fordpass_token.txt" #Name of the file for the user_fordpass_token.txt from the fordpass-ha integration
+
+
+def get_autonomic_token(ford_access_token):
+    url = "https://accounts.autonomic.ai/v1/auth/oidc/token"
+    headers = {
+        "accept": "*/*",
+        "content-type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "subject_token": ford_access_token,
+        "subject_issuer": "fordpass",
+        "client_id": "fordpass-prod",
+        "grant_type": "urn:ietf:params:oauth:grant-type:token-exchange",
+        "subject_token_type": "urn:ietf:params:oauth:token-type:jwt"
+    }
+
+    try:
+        response = requests.post(url, headers=headers, data=data)
+        response.raise_for_status()
+        autonomic_token_data = response.json()
+        return autonomic_token_data
+
+    except requests.exceptions.HTTPError as errh:
+        print(f"HTTP Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")
+    except requests.exceptions.RequestException as err:
+        print(f"Something went wrong: {err}")
+
+def get_vehicle_status(vin, access_token):
+    BASE_URL = "https://api.autonomic.ai/"
+    endpoint = f"v1beta/telemetry/sources/fordpass/vehicles/{vin}:query"
+    url = f"{BASE_URL}{endpoint}"
+    headers = {
+        "Authorization": f"Bearer {access_token}",  # Replace 'your_autonom_token' with the actual Autonomic API token
+        "Content-Type": "application/json",
+        "accept": "*/*"
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json={})
+        response.raise_for_status()  # Raise HTTPError for bad requests (4xx and 5xx status codes)
+
+        # Parse the JSON response
+        vehicle_status_data = response.json()
+        return vehicle_status_data
+
+    except requests.exceptions.HTTPError as errh:
+        print(f"HTTP Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")
+    except requests.exceptions.RequestException as err:
+        print(f"Something went wrong: {err}")
+
+# Get FordPass token
+with open(fp_token, 'r') as file:
+    fp_token_data = json.load(file)
+
+ford_access_token = fp_token_data['access_token']
+
+# Exchange Fordpass token for Autonomic Token
+autonomic_token = get_autonomic_token(ford_access_token)
+vehicle_status = get_vehicle_status(fp_vin, autonomic_token["access_token"])
+
+# Write the updated JSON data to the file
+with open('new_data.json', 'w') as file:
+    json.dump(vehicle_status, file, indent=4)
+print("done")
+
