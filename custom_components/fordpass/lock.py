ccc4360fac2fc881f221d087b8def24bcdb03aea
steve <steve@itchannel.me>
2023-10-10 19:44:59 +1000

    updated locks and switch

--- custom_components/fordpass/lock.py
+++ custom_components/fordpass/lock.py
@@ -14,10 +14,11 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     entry = hass.data[DOMAIN][config_entry.entry_id][COORDINATOR]
 
     lock = Lock(entry)
-    if lock.coordinator.data.get("lockStatus", {}) and lock.coordinator.data["lockStatus"]["value"] != "ERROR":
+    if lock.coordinator.data["metrics"]["doorLockStatus"] and lock.coordinator.data["metrics"]["doorLockStatus"][0]["value"] != "ERROR":
         async_add_entities([lock], False)
     else:
         _LOGGER.debug("Ford model doesn't support remote locking")
+
 
 
 class Lock(FordPassEntity, LockEntity):
@@ -25,32 +26,37 @@ class Lock(FordPassEntity, LockEntity):
 
     def __init__(self, coordinator):
         """Initialize."""
-        super().__init__(
-            device_id="fordpass_doorlock",
-            name="fordpass_doorlock",
-            coordinator=coordinator,
-        )
+        self._device_id="fordpass_doorlock"
+        self.coordinator = coordinator
+
+        self.data = coordinator.data["metrics"]
+
+        # Required for HA 2022.7
+        self.coordinator_context = object()
 
     async def async_lock(self, **kwargs):
         """Locks the vehicle."""
         self._attr_is_locking = True
         self.async_write_ha_state()
         _LOGGER.debug("Locking %s", self.coordinator.vin)
-        await self.coordinator.hass.async_add_executor_job(
+        status = await self.coordinator.hass.async_add_executor_job(
             self.coordinator.vehicle.lock
         )
+        _LOGGER.debug(status)
         await self.coordinator.async_request_refresh()
+        _LOGGER.debug("Locking here")
         self._attr_is_locking = False
         self.async_write_ha_state()
 
     async def async_unlock(self, **kwargs):
         """Unlocks the vehicle."""
+        _LOGGER.debug("Unlocking %s", self.coordinator.vin)
         self._attr_is_unlocking = True
         self.async_write_ha_state()
-        _LOGGER.debug("Unlocking %s", self.coordinator.vin)
-        await self.coordinator.hass.async_add_executor_job(
+        status = await self.coordinator.hass.async_add_executor_job(
             self.coordinator.vehicle.unlock
         )
+        _LOGGER.debug(status)
         await self.coordinator.async_request_refresh()
         self._attr_is_unlocking = False
         self.async_write_ha_state()
@@ -58,9 +64,9 @@ class Lock(FordPassEntity, LockEntity):
     @property
     def is_locked(self):
         """Determine if the lock is locked."""
-        if self.coordinator.data is None or self.coordinator.data["lockStatus"] is None:
+        if self.data is None or self.data["doorLockStatus"] is None:
             return None
-        return self.coordinator.data["lockStatus"]["value"] == "LOCKED"
+        return self.data["doorLockStatus"][0]["value"] == "LOCKED"
 
     @property
     def icon(self):
